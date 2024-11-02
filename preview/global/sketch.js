e5c9f77faa16e0efc7c5efa06f6b7ea2ff6266b3
Diana Galindo <dianamilena24@gmail.com>
2024-11-02 20:08:03 +0000

    test preview sketch

--- preview/global/sketch.js
+++ preview/global/sketch.js
@@ -1,16 +1,16 @@
 //console.log(p5);
-
 function setup(){
   createCanvas(200, 200);
 }
 
 async function draw(){
-  background(0, 50, 50);
+  background(255);
   circle(100, 100, 50);
 
-  let color = new color("hsl", [10,11,14]);
-
-  fill(color);
+  //let c1 = new Color("hsl", [10,11,14]);
+ 
+  let c1 = color(255,204,0);
+  fill(c1);
   textSize(30);
   text('hello', 10, 30);
 }
